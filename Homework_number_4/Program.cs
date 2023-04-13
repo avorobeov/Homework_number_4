97cb73f04d0a6b3a1f6774a1ea13f8654bc13085
воробйов александр <avorobeov21@gmail.com>
2023-04-14 00:01:56 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesPerRow = 3;
+            int totalPictures = 52;
+            int fullRows;
+            int remainingPictures;
+
+            fullRows = totalPictures / picturesPerRow;
+            remainingPictures = totalPictures % picturesPerRow;
+
+            Console.WriteLine($"Количество полных рядов: {fullRows}\nОстаток картинок: {remainingPictures}");
+            Console.ReadKey();
+        }
+    }
+}
